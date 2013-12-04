18471194904963c0606cb45ea36d8edfe44a55c1
vebzical <vebzical@hotmail.com>
2013-12-04 17:12:18 +0200

    Clientin login tehty
    Kivaa

---
+++ YourMovies_VisualStudioProjekti/Elokuvatietue_v8/Elokuvatietue/Ikkunat/Login.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Elokuvatietue.Ikkunat
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public string userName { get; set; }
+        public Login()
+        {
+            InitializeComponent();
+            myIni();
+        }
+
+        private void myIni()
+        {
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (authenticateUser())
+            {
+                userName = txtUsername.Text;
+                this.Close();
+            }
+            else
+            {
+                txtUsername.Foreground = Brushes.Red;
+                txtPassword.Foreground = Brushes.Red;
+                txtUsername.Text = "DENIED!";
+            }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            //Cancel
+            this.Close();
+        }
+
+        private bool authenticateUser() 
+        {
+            var authService = new AuthenticationService.AuthenticationServiceClient();
+
+            try
+            {
+                if (authService.Login(txtUsername.Text.ToString(), txtPassword.Password.ToString(), string.Empty, true))
+                {
+                    return true;
+                }
+
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
